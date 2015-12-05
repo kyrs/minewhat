f962d8fdcca09d5171ddd2fd4c17cd9357bc20cb
ks <kumarshubham652@gmail.com>
2015-12-05 19:44:57 +0530

    question 2_modify

--- question2.py
+++ question2.py
@@ -11,19 +11,22 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from collections import Counter
 import numpy as np 
-import sklearn 
+from sklearn import linear_model
 
-def features_ext(cat,m09,m10,m11,feature_words) :
+def features_ext(cat,m10,m11,feature_words) :
     feature={}
     for w in feature_words :
         if w in cat :
             feature[w] = 1
         else:
             feature[w]=0
-    feature["m09"]= m09
-    feature["m10"] = m10
-    feature["m11"] = m11
+    if not np.isnan(m10):
+        feature["m10"] = m10
+    else:
+        feature["m10"] = 0
 
+    feature["m11"] = m11
+    #print feature
     return feature
 
 
@@ -52,9 +55,31 @@ temp = Counter(category_count)
 
 feature_words = [t for t in temp.keys() if temp[t]>5]
 
-train_dataset = []
+dataset = []
 ##creating Trainig Data
-for i in range(1,346):
+#print np.isnan(month_m08[40])
+for i in range(1,442):
     cat = json.loads(category[i])
-    if month_m08[i] is not "NAN":
-        train_dataset.append((tuple(features_ext(cat,month_m09[i],month_m10[i],month_m11[i],feature_words)),month_m08[i]))
+    if not np.isnan(month_m08[i]):
+        f= features_ext(cat,month_m10[i],month_m11[i],feature_words)
+        f["VALUE"] = month_m08[i]
+        dataset.append(f)
+    else:
+        f = features_ext(cat,month_m10[i],month_m11[i],feature_words)
+        f["VALUE"] = -1
+        dataset.append(f)
+
+dataset = pd.DataFrame(dataset)
+value = dataset["VALUE"]
+del dataset["VALUE"]
+
+#dataset.drop("VALUE")
+train_data = dataset.ix[0:347,:]
+test_data = dataset.ix[347:,:]
+# train_data_frame
+model = linear_model.LinearRegression()
+model.fit(train_data,value.ix[0:347])
+y=model.predict(test_data)
+for i in range(347,len(month_m08)-1):
+    month_m08.ix[i]= int(abs(y[i-347] ))
+print month_m08
